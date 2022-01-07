63c01efbd043d4046e1073b8b1fd427da06da5e5
IuliiaKonovalova <juliiakonn@protonmail.com>
2022-01-07 12:31:11 +0800

    Add modules.py with class Category

---
+++ task_manager/modules.py
@@ -0,0 +1,10 @@
+from task_manager import db
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship("Task", backref="category", cascade="all, delete", lazy=True)
+    
+    def __repr__(self):
+        return self.category_name
